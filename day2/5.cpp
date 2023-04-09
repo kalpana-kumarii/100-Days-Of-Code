edcd358c44086abd7a6a6a04699e79e2b9c8b904
Kalpana  Kumari <84411818+kalpana-kumarii@users.noreply.github.com>
2023-04-09 19:48:33 +0530

    Create 5.cpp

---
+++ day2/5.cpp
@@ -0,0 +1,34 @@
+//https://practice.geeksforgeeks.org/problems/find-minimum-and-maximum-element-in-an-array4428/1?page=1&difficulty[]=-1&category[]=Arrays&sortBy=submissions
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+#define ll long long
+
+pair<long long, long long> getMinMax(long long a[], int n) ;
+
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        ll a[n];
+        for (int i = 0; i < n; i++) cin >> a[i];
+
+        pair<ll, ll> pp = getMinMax(a, n);
+
+        cout << pp.first << " " << pp.second << endl;
+    }
+    return 0;
+}
+// } Driver Code Ends
+
+
+pair<long long, long long> getMinMax(long long a[], int n) {
+    sort(a,a+n);
+   
+   return {a[0],a[n-1]};
+    
+ 
+    
+}
