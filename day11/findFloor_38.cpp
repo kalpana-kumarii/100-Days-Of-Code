a3d822bcead16400c1320276e803cb2dddf2a92d
Kalpana  Kumari <84411818+kalpana-kumarii@users.noreply.github.com>
2023-04-27 21:59:28 +0530

    Create findFloor_38.cpp

---
+++ day11/findFloor_38.cpp
@@ -0,0 +1,70 @@
+//https://practice.geeksforgeeks.org/problems/floor-in-a-sorted-array-1587115620/1?page=1&difficulty[]=0&category[]=Searching&sortBy=submissions
+//Floor in a Sorted Array
+
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// } Driver Code Ends
+class Solution{
+  public:
+    // Function to find floor of x
+    // n: size of vector
+    // x: element whose floor is to find
+    int findFloor(vector<long long> v, long long n, long long x){
+        
+        // Your code here
+        long long s=0;
+        long long e=n-1;
+        int mid;
+        int ans=-1;
+        while(s<=e){
+            mid=s+(e-s)/2;
+            
+            if(v[mid]<x){
+                ans=mid;
+                s=mid+1;
+            }
+            else if(v[mid]>x){
+                e=mid-1;
+            }
+            else{
+                return mid;
+            }
+        }
+        
+        return ans;
+        
+    }
+};
+
+
+//{ Driver Code Starts.
+
+int main() {
+	
+	long long t;
+	cin >> t;
+	
+	while(t--){
+	    long long n;
+	    cin >> n;
+	    long long x;
+	    cin >> x;
+	    
+	    vector<long long> v;
+	    
+	    for(long long i = 0;i<n;i++){
+	        long long temp;
+	        cin >> temp;
+	        v.push_back(temp);
+	    }
+	    Solution obj;
+	    cout << obj.findFloor(v, n, x) << endl;
+	   
+	}
+	
+	return 0;
+}
+// } Driver Code Ends
