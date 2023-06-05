7466b7f6b7b0f466bb38f7c08c522545a5169ac3
Kalpana  Kumari <84411818+kalpana-kumarii@users.noreply.github.com>
2023-06-06 00:01:53 +0530

    Create removeDuplicates.cpp

---
+++ day18/removeDuplicates.cpp
@@ -0,0 +1,91 @@
+//Remove duplicate element from sorted Linked List
+//https://practice.geeksforgeeks.org/problems/remove-duplicate-element-from-sorted-linked-list/1?page=1&difficulty[]=0&category[]=Linked%20List&sortBy=submissions
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+};
+
+
+void print(Node *root)
+{
+    Node *temp = root;
+    while(temp!=NULL)
+    {
+    cout<<temp->data<<" ";
+    temp=temp->next;
+    }
+}
+Node* removeDuplicates(Node *root);
+int main() {
+	// your code goes here
+	int T;
+	cin>>T;
+
+	while(T--)
+	{
+		int K;
+		cin>>K;
+		Node *head = NULL;
+        Node *temp = head;
+
+		for(int i=0;i<K;i++){
+		int data;
+		cin>>data;
+			if(head==NULL)
+			head=temp=new Node(data);
+			else
+			{
+				temp->next = new Node(data);
+				temp=temp->next;
+			}
+		}
+		
+		Node *result  = removeDuplicates(head);
+		print(result);
+		cout<<endl;
+	}
+	return 0;
+}
+// } Driver Code Ends
+
+
+/*
+struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+};*/
+
+//Function to remove duplicates from sorted linked list.
+Node *removeDuplicates(Node *head)
+{
+ // your code goes here
+ 
+Node *slow=head;
+Node *fast=head->next;
+while(fast!=NULL){
+    if(slow->data==fast->data){
+        slow->next=fast->next;
+        Node *temp=fast;
+        fast=fast->next;
+        delete temp;
+    }
+    else{
+        slow=slow->next;
+        fast=fast->next;
+    }
+}
+//  cout<<head->data<<" "<<t->data<<endl;
+ return head;
+}
