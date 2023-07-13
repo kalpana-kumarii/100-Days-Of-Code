61ef4be6c7e3fa9db405a4687c1e8ebb25a7f1f4
Kalpana  Kumari <84411818+kalpana-kumarii@users.noreply.github.com>
2023-07-13 23:01:00 +0530

    Create isAnagram.cpp

---
+++ day20/isAnagram.cpp
@@ -0,0 +1,27 @@
+//https://leetcode.com/problems/valid-anagram/description/
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+   unordered_map<char,int>mp;
+
+   for(auto x:s){
+       mp[x]++;
+      
+   }
+
+    for(auto x:t){
+        mp[x]--;
+    }
+
+for(auto x:mp){
+    
+    if(x.second!=0){
+        return false;
+    }
+}
+
+return true;
+    }
+
+};
