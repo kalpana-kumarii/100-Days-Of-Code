ebe175a0996303f17dd9c2c899d1c7f7a4cbb859
Kalpana  Kumari <84411818+kalpana-kumarii@users.noreply.github.com>
2023-10-18 22:16:58 +0530

    Update search.cpp

--- day30/search.cpp
+++ day30/search.cpp
@@ -1,1 +1,17 @@
-/
+//https://practice.geeksforgeeks.org/problems/search-a-node-in-bst/1?page=1&difficulty[]=-1&category[]=Binary%20Search%20Tree&sortBy=submissions
+
+bool search(Node* root, int x) {
+    // Your code here
+    
+    if(!root){
+        return 0;
+    }
+    if(root->data==x){
+        return 1;
+    }
+    if(root->data>x ){
+        search(root->left,x);
+    }
+    else{
+        search(root->right,x);
+    
